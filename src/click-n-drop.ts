173f549f577fd75b30357541e923783b1bd0ee84
vladyn <vlado@vlado1.com>
2019-04-05 15:08:29 +0300

    Add(gameplay):A modal dialog polyfill. Refactoring.

--- src/click-n-drop.ts
+++ src/click-n-drop.ts
@@ -1,10 +1,11 @@
-const clickNDrop = (col:any, player:string, gameState: boolean) => {
-  if (col.querySelectorAll('span').length === 6 && !gameState) {
+const clickNDrop = (col:any, player:string, yourTurn: boolean) => {
+  if (col.querySelectorAll('span').length === 6 && !yourTurn) {
     return false;
   }
   const token: HTMLElement = document.createElement('span');
   token.setAttribute('class', `flc-game-piece ${player}`);
   col.prepend(token);
+  return true;
 };
 
 export default clickNDrop;
