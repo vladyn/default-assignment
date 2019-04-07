16294ddffe7c428c1f1d31eda4c83145055e329e
vladyn <vlado@vlado1.com>
2019-04-07 10:26:53 +0300

    Fix(gameplay)Pronouncing a Drawgame. Limiting clinet connetion.

--- src/clientConnector.ts
+++ src/clientConnector.ts
@@ -1,6 +1,6 @@
 import { createClient } from '../lib/websocketConnector';
 
-const client = createClient('192.168.0.104', 4000);
+const client = createClient('localhost', 4000);
 const connection = client.connect({
   name: localStorage.getItem('username'),
   gender: localStorage.getItem('gender')
