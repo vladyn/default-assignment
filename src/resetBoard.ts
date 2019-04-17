72242e233ae50ba676f2df89dc40422c78364da3
vladyn <vlado@vlado1.com>
2019-04-17 14:49:55 +0300

    Fix(gameplay):restoring the board and setting a proper player role. Bigfixes.

--- src/resetBoard.ts
+++ src/resetBoard.ts
@@ -1,10 +1,15 @@
 const cols: NodeListOf<Element> = document.querySelectorAll('div.board-col');
+const hostPlayerAvatar: HTMLSpanElement = document.getElementById('hostPlayerAvatar');
+const guestPlayerAvatar: HTMLSpanElement = document.getElementById('guestPlayerAvatar');
 const resetBoard = () => {
+  const winnerClass: string = 'winner';
   cols.forEach((el: any) => {
     while (el.firstChild) {
       el.firstChild.remove();
     }
   });
+  hostPlayerAvatar.classList.remove(winnerClass);
+  guestPlayerAvatar.classList.remove(winnerClass);
 };
 
 export default resetBoard;
