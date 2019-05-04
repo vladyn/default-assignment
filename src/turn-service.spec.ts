0748cc2fdb83ce3bf2e293b9fa1f452f3672f2ed
vladyn <vlado@vlado1.com>
2019-05-04 15:11:53 +0300

    Fix(pipeline)Adding the first test and mocking some browser features.

--- src/turn-service.spec.ts
+++ src/turn-service.spec.ts
@@ -1,18 +1,31 @@
 import turnService from './turn-service';
-const chai = require('chai');
-const sinon = require('sinon');
-const sinonChai = require('sinon-chai');
-
-require('jsdom-global')();
-
-chai.use(sinonChai);
-const {
-  expect, use, should, assert,
-} = require('chai');
 
 describe('Turn service', () => {
-  it('should return a correct player turn', function () {
-    const plyerMove = 'player-tow';
-    console.log(turnService());
+  const turn = jest.fn();
+  beforeEach(() => {
+    window.location.assign = jest.fn();
+    turn
+      .mockReturnValueOnce('player-one')
+      .mockReturnValueOnce('player-two');
+  });
+  afterEach(() => {
+  });
+  it('should call the turn service', () => {
+    turnService();
+    expect(turnService()).toEqual('player-one');
+  });
+  it('should check the times return service is called', () => {
+    turn();
+    expect(turn.mock.calls.length).toBe(1);
+    turn();
+    expect(turn.mock.calls.length).toBe(2);
+  });
+  it('should return the opponent player: player-one', () => {
+    turn();
+    expect(turn.mock.results[0].value).toBe('player-one');
+  });
+  it('should return the opponent player: player-two', () => {
+    turn();
+    expect(turn.mock.results[1].value).toBe('player-two');
   });
 });
