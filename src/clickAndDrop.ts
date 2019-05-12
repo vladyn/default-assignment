05d9ae7da6afd41dec317168afcf0fdb6b25037c
vladyn <vlado@vlado1.com>
2019-05-12 18:17:20 +0300

    Fix(pipeline)Unit tests for the click and drop service

--- src/clickAndDrop.ts
+++ src/clickAndDrop.ts
@@ -1,6 +1,8 @@
-const clickAndDrop = (col: HTMLElement | Element, player: string, yourTurn?: boolean):
-  HTMLElement | Element | boolean => {
-  if (col.querySelectorAll('span').length === 6 && yourTurn !== true) return false;
+const clickAndDrop = (col: HTMLElement | Element, player: string, yourTurn: boolean):
+  HTMLElement | Element => {
+  if (col.querySelectorAll('span').length >= 6 || yourTurn !== true) {
+    return col;
+  }
   const token: HTMLElement = document.createElement('span');
   token.setAttribute('class', `flc-game-piece ${player}`);
   col.prepend(token);
